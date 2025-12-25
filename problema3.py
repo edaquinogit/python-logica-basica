2b128fddcff1e843d0da34374642b93cb7848c82
Ednaldo Aquino <ednaldoaquino87@gmail.com>
2025-12-24 22:17:07 -0300

    verificação de saldo
    exercicio de verificação de saldo

---
+++ problema3.py
@@ -0,0 +1,16 @@
+# Exercicio de verificação de Saldo Bancario
+
+nome = input('Digite seu nome:')
+
+print('Ola{}, Seja bem vindo ao sistema bancario.'.format(nome)) 
+saldo = float(input('Digite o saldo da sua conta bancaria: RS'))
+print()
+ 
+if saldo >= 0:
+    print('Seu saldo é positivo! Saldo: RS{:.2f}'.format(saldo))
+else:
+    print('Seu Saldo é negativo! Saldo: RS{:.2f}'.format(saldo))
+
+print()   
+
+print('Obrigado por ultilizar nosso sistema bancario!')
